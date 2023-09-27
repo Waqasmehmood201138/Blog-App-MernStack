2ef8d7ae87bd3c6958ccabbc7f1526e62ca1a72d
Waqas Mehmood <waqmeh456@gmail.com>
2023-09-28 02:47:24 +0500

    categories bar completed

--- readit/src/App.js
+++ readit/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  LoginPage  from './Auth/LoginPage';
 import SignupPage from './Auth/SignupPage';
---
+++ readit/src/navbar/CategoriesBar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+
+export default function CategoriesBar() {
+
+
+    const [isSearchActive, setIsSearchActive] = useState(false)
+
+    const handleSearch = () => {
+        setIsSearchActive(true)
+    }
+    const handleClose = () => {
+        setIsSearchActive(false)
+    }
+
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="row p-3">
+                        {isSearchActive ?
+                            (<>
+                                <div className="col-10">
+                                    <form action="" className='d-flex justify-content-center'>
+                                        <input type="text" className='form-control' />
+                                        <input onClick={handleClose} type="submit" value="close" className='btn btn-danger ms-1'/>
+                                    </form>
+                                </div></>)
+                            :
+                            (<><div className="col-2 book_cat">Free Tech Books</div>
+                                <div className="col categories_name">JavaScript</div>
+                                <div className="col-2 categories_name">HTML & CSS</div>
+                                <div className="col categories_name">AI</div>
+                                <div className="col categories_name">Java</div>
+                                <div className="col categories_name">Wordpress</div>
+                                <div className="col categories_name">PHP</div>
+                                <div className="col categories_name">Python</div>
+                            </>)
+                        }
+                        <div className="col btn btn-secondary" onClick={handleSearch}>Search</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
