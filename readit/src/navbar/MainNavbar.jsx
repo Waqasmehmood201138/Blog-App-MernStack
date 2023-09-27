52bf78af16d13dd5b39356497f481605e9c4a3aa
Waqas Mehmood <waqmeh456@gmail.com>
2023-09-27 22:12:21 +0500

    main navbar completed

--- readit/src/navbar/MainNavbar.jsx
+++ readit/src/navbar/MainNavbar.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MainNavbar = () => {
   return (
     <div>
-     <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <div className='ms-auto d-md-flex'>
-      <form className="d-flex justify-content-end" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">Readit!</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className='ms-auto d-md-flex'>
+              <Link to="/"><button className='btn btn-dark fw-bold'>Login</button></Link>
+              <Link to="/signup"><button className='btn btn-dark fw-bold ms-3' style={{ backgroundColor: "#424649" }}>Signup</button></Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
