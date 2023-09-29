c27ef3ba826c8d7f7dc226efdc007ec214a0c288
itzsaad8 <msikhan32@gmail.com>
2023-09-29 15:01:55 -0700

    login signup final completed

--- readit/src/Auth/LoginPage.jsx
+++ readit/src/Auth/LoginPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Auth.css'
 import { Link } from 'react-router-dom'
 
 export default function LoginPage() {
+    const [email,setEmail]= useState("")
+    const [password,setPassword]= useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email,password)
+        setEmail(email)
+        setPassword(password)
+    }
     return (
         <>
 
@@ -11,17 +20,28 @@ export default function LoginPage() {
                     <div className="col">
 
 
-                        <form className="form">
+                        <form className="form" onSubmit={handleSubmit}>
                             <p className="form-title">Sign in to your account</p>
                             <div className="input-container">
-                                <input type="email" placeholder="Enter email" />
+                                <input 
+                                type="email" 
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e)=>{setEmail(e.target.value)}} />
                                 <span>
                                 </span>
                             </div>
                             <div className="input-container">
-                                <input type="password" placeholder="Enter password" />
+                                <input 
+                                type="password"
+                                 placeholder="Enter password"
+                                 value={password}
+                                 onChange={(e)=>{setPassword(e.target.value)}} />
                             </div>
-                            <button type="submit" className="submit">
+                            <button
+                             type="submit" 
+                             className="submit" 
+                             >
                                 Sign in
                             </button>
 
